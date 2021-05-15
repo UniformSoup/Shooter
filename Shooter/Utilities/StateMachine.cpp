57dea5d9aaeef05fbfac78b1969c2b16c2ad19fd
UniformSoup <halad@outlook.com>
2021-05-15 23:05:45 +0100

    Major Statemachine Improvement

--- Shooter/Utilities/StateMachine.cpp
+++ Shooter/Utilities/StateMachine.cpp
@@ -4,36 +4,43 @@ size_t StateMachine::size() const
 {
 	return states.size();
 }
+
 GameState& StateMachine::getCurrentState() const 
 {
 	return *states.top();
 }
+
 void StateMachine::addState(GameState* s)
 {
 	newState = std::shared_ptr<GameState>(s);
 	flag = Flag::ADD;
 }
+
 void StateMachine::replaceState(GameState* s)
 {
 	newState = std::shared_ptr<GameState>(s);
 	flag = Flag::REPLACE;
 }
+
 void StateMachine::removeState() { flag = Flag::REMOVE; }
+
+/* can throw if you try to remove without checking stack size */
 void StateMachine::updateState()
 {
 	switch (flag)
 	{
+	case Flag::REPLACE:
+		states.pop(); // purposely bleed through here.
 	case Flag::ADD:
 		states.push(newState);
+		newState = nullptr;
+		// this is important,
+		// so we are not holding on to something
+		// that we are not using.
 		break;
 
 	case Flag::REMOVE:
 		states.pop();
-		break;
-
-	case Flag::REPLACE:
-		states.pop();
-		states.push(newState);
 		break;
 
 	default:
